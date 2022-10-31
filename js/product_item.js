01f32c1ce15ae6e89f65b622829031f2d22a44fe
chubbyrabi <chubbytobey@gmail.com>
2022-10-31 23:31:38 +0800

    fix all

--- js/product_item.js
+++ js/product_item.js
@@ -8,18 +8,52 @@
 
 
 
+//  計算元素 top (有定位或旋轉時)
+function getOffsetTop(element){
+    // var offsetLeft = 0;
+    var offsetTop  = 0;
+    while(element){
+        // offsetLeft += element.offsetLeft;
+        offsetTop  += element.offsetTop;
+        element = element.offsetParent;
+    }
+    // return [offsetLeft, offsetTop];
+    return [offsetTop];
+}
+
+
+
+// 小圖換大圖
+function showBig(e){
+    let small = e.target;
+    document.getElementById('big').src = small.src;
+}
+function init(){
+    let imgs = document.querySelectorAll('.pic-small img');
+    for(let i=0; i<imgs.length; i++){
+        imgs[i].onclick = showBig;
+    }
+}
+window.addEventListener('load', init, false);
+
+
+
 // click 事件
-// $(function click(){
-//     $('.js-click').click(function(){
-//         $(this).toggleClass('on');
-//     });
-// });
+$(function click(){
+    $('.js-click').click(function(){
+        $(this).toggleClass('on');
+    });
+});
 
 
 
 // 捲動螢幕事件
 $(function scroll(){
     $(window).on('scroll', function (){
+        let scrTop = $(window).scrollTop();
+        let winH = $(window).height();
+        let winW = $(window).width();
+
         // header 捲動出現影子
         if( $(window).scrollTop() > 0){
             $('header').addClass('js-hd-shadow');
@@ -27,6 +61,32 @@ $(function scroll(){
         else{
             $('header').removeClass('js-hd-shadow');
         }
+
+        // 物件出現的動畫
+        $('.js-scroll').each(function(){
+            // let objTop = getOffsetTop(this);
+            // console.log(objTop)
+            if( winW < 768 ){
+                let objTop = getOffsetTop(this);
+                if (scrTop > objTop - winH + winH / 8){
+                    $(this).removeClass('out');
+                    $(this).addClass('in');
+                }
+                else{
+                    $(this).addClass('out');
+                }
+            }
+            else if(winW >= 768){
+                let objTop = getOffsetTop(this);
+                if (scrTop > objTop - winH + winH / 5){
+                    $(this).removeClass('out');
+                    $(this).addClass('in');
+                }
+                else{
+                    $(this).addClass('out');
+                }
+            }
+        });
     });
 });
 
